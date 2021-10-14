22459b3d6b5f2210567733ffaca6e2408e8a23d6
Shehroz Amjad <shehrozaaa@gmail.com>
2021-10-14 16:03:47 +0500

    route generate agin

--- rampit/lib/core/router.dart
+++ rampit/lib/core/router.dart
@@ -7,15 +7,9 @@ import 'package:rampit/views/address/address_view.dart' as view0;
 import 'package:rampit/views/closed/closed_view.dart' as view1;
 import 'package:rampit/views/home/home_view.dart' as view2;
 import 'package:rampit/views/category/category_view.dart' as view3;
+import 'package:rampit/views/product/product_view.dart' as view4;
 
 class Router {
-
-  int _currentIndex;
-  int get currentIndex => this._currentIndex;
-  set currentIndex(int value) {
-    this._currentIndex = value;
-  }
-
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case addressViewRoute:
@@ -26,6 +20,8 @@ class Router {
         return MaterialPageRoute(builder: (_) => view2.HomeView());
       case categoryViewRoute:
         return MaterialPageRoute(builder: (_) => view3.CategoryView());
+      case productViewRoute:
+        return MaterialPageRoute(builder: (_) => view4.ProductView());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
